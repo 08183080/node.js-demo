aa3ab24b7e7a7be7554675fc2626c968bf446772
龙龙 <51738561+08183080@users.noreply.github.com>
2023-10-25 21:03:57 +0800

    hello node.js

---
+++ demos/hello.js
@@ -0,0 +1,22 @@
+/**
+ * 2023/10/25
+ * write by chatgpt...
+ */
+
+// 引入 http 模块
+const http = require('http');
+
+// 创建服务器
+const server = http.createServer((req, res) => {
+  // 设置响应头
+  res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+
+  // 发送响应内容
+  res.end('Hello, World!\n');
+});
+
+// 监听端口 3000
+server.listen(3000, 'localhost', () => {
+  console.log('Server running at http://localhost:3000/');
+});
---
+++ demos/hello.md
@@ -0,0 +1,3 @@
+# node.js入门与使用
+hello.js在3000端口开启一个应用
+![Alt text](image.png)
